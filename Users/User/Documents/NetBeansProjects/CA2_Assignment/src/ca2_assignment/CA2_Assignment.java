deb0d850deffe9c1779c95e758c23e6e481e2d60
Pavel <iverick091@gmail.com>
2024-12-12 12:29:53 +0000

    Initial setup of the project

---
+++ Users/User/Documents/NetBeansProjects/CA2_Assignment/src/ca2_assignment/CA2_Assignment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca2_assignment;
+
+/**
+ *
+ * @author User
+ */
+public class CA2_Assignment {
+
+    /**
+     * Link to Github repo - https://github.com/Iverick/CA2_Assignment
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
